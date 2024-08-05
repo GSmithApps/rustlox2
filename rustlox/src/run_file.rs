015dea57f4630697bf0770e89f2b4852ebb8a517
GSmithApps <14.gsmith.14@gmail.com>
2024-08-04 23:03:15 -0500

    amaze

--- rustlox/src/run_file.rs
+++ rustlox/src/run_file.rs
@@ -2,7 +2,7 @@
 //! 
 //! Requires on the `run` function.
 
-use std::path::PathBuf;
+use std::path::{self, PathBuf};
 use std::io::Write;
 
 
@@ -20,53 +20,64 @@ use std::io::Write;
 /// the file, we'll go into a loop asking the user to try
 /// again.  If they type in `exit` or `quit`, then we'll
 /// break the loop.
-pub fn run_file(path_string: &str) {
+pub fn run_file(first_path_string: &str) {
 
-    let mut i = 1;
+    let mut is_first_time = true;
 
     let mut string_from_file: Result<String, std::io::Error>;
 
-    loop {
-        if i == 1 {
+    let mut path_string: String;
 
-            let path: PathBuf = PathBuf::from(path_string);
+    loop {
+        if is_first_time {
+
+            path_string = first_path_string.to_string();
+
+            let path: PathBuf = PathBuf::from(&path_string);
             string_from_file = std::fs::read_to_string(path);
+
+            is_first_time = false;
 
         } else {
 
-            let lines_of_statements_in_response = vec![
-                "There was an error reading the file.",
-                "Please try again.",
-                "Type 'exit' or 'quit' to exit.",
-                // &format!("{:?}", e),
-            ];
-            utilities::print_with_surrounding_box::print_with_surrounding_box(lines_of_statements_in_response);
-        
             print!("> ");
             std::io::stdout().flush().unwrap();
-        
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-        
-            if input.trim() == "exit" || input.trim() == "quit" {
+
+            path_string = String::new();
+            
+            std::io::stdin().read_line(&mut path_string).unwrap();
+            
+            if path_string.trim() == "exit" || path_string.trim() == "quit" {
                 break;
             }
-            string_from_file = std::fs::read_to_string(input.trim());
-
+            string_from_file = std::fs::read_to_string(path_string.trim());
+            
         }
-
-        i = i + 1;
-
+        
         match string_from_file {
             Ok(string_from_file) => {
                 crate::run::run(&string_from_file);
-                break;
+                let lines_of_statements_in_response = vec![
+                    "That seemed to work!",
+                    "If you want to run more, pass another file path.",
+                    "Type 'exit' or 'quit' to exit.",
+                ];
+                utilities::print_with_surrounding_box::print_with_surrounding_box(lines_of_statements_in_response);
             },
             Err(e) => {
-                continue;
+                let error_statement = format!("{:?}", e);
+                let lines_of_statements_in_response = vec![
+                    "There was an error reading the file:",
+                    &path_string.trim(),
+                    "Please try again.",
+                    "Type 'exit' or 'quit' to exit.",
+                    &error_statement,
+                ];
+                utilities::print_with_surrounding_box::print_with_surrounding_box(lines_of_statements_in_response);
             }
         }
-    }
 
+    }
+    
 }
 
