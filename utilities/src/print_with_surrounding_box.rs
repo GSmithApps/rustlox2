044619b33518859c7f13ff3b7d5f81c90410e9cc
GSmithApps <14.gsmith.14@gmail.com>
2024-11-17 14:26:22 -0600

    cleaning a function

--- utilities/src/print_with_surrounding_box.rs
+++ utilities/src/print_with_surrounding_box.rs
@@ -49,34 +49,16 @@ pub fn print_with_surrounding_box(lines_of_statements_in_response: Vec<&str>) {
 /// ]);
 /// ```
 ///
-fn get_surrounding_box(lines_of_statements_in_response: Vec<&str>) -> Vec<String> {
-
-    // make a vector of strings that we can push onto
-    let mut lines_with_surrounding_box = Vec::<String>::new();
-
-    // map or iterate through the vector of lines and return the greatest length
-    let max_of_lengths = lines_of_statements_in_response.iter().map(|line| line.len()).max();
-
-    // handle the option such that if there is no max length, we can set it to 0,
-    // otherwise, we can use the value
-    let max_len = max_of_lengths.unwrap_or(0);
-
-    // make a vector of filler spaces to add to the end of each line
-    let filler_spaces_for_statements = lines_of_statements_in_response.iter().map(
-        |line| " ".repeat(max_len - line.len())
-    ).collect::<Vec<String>>();
-
+fn get_surrounding_box(lines_of_text: Vec<&str>) -> Vec<String> {
+    
+    let max_len = lines_of_text.iter().map(|line| line.len()).max().unwrap_or(0);
+    
     let horizontal_lines = "─".repeat(max_len);
 
-    // push the first item onto the vector
-    lines_with_surrounding_box.push(format!("┌─{}─┐", horizontal_lines));
-
-    for (line, filler) in lines_of_statements_in_response.iter().zip(filler_spaces_for_statements.iter()) {
-        lines_with_surrounding_box.push(format!("│ {}{} │",line, filler));
-    }
-    lines_with_surrounding_box.push(format!("└─{}─┘", horizontal_lines));
-
-    lines_with_surrounding_box
+    std::iter::once(
+        format!("┌─{}─┐", horizontal_lines).into()).chain(
+        lines_of_text.iter().map(|line| format!("│ {}{} │", line, " ".repeat(max_len - line.len())))).chain(
+        std::iter::once(format!("└─{}─┘", horizontal_lines).into())).collect()
 }
 
 #[cfg(test)]
